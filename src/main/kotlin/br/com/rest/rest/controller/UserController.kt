c404baf5c303bebb4cc64e3d0a1a7d581e91dfdf
Bruno Medeiros <bruno.menescal@contabilizei.com.br>
2022-08-20 02:08:39 -0300

    Finalizado padrão controller

--- src/main/kotlin/br/com/rest/rest/controller/UserController.kt
+++ src/main/kotlin/br/com/rest/rest/controller/UserController.kt
@@ -1,45 +1,52 @@
 package br.com.rest.rest.controller
 
+
+import br.com.rest.rest.controller.UserController.Companion.REQUEST
 import br.com.rest.rest.dto.SingUpDTO
 import br.com.rest.rest.dto.UpdateUserDTO
 import br.com.rest.rest.dto.UserDTO
-import br.com.rest.rest.model.User
 import br.com.rest.rest.service.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.util.UriBuilder
 import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping(REQUEST)
 class UserController(
     val userService: UserService
 ) {
+    companion object{
+        const val REQUEST = "/user"
+        const val SING_UP = "/singup"
+        const val ID = "/{id}"
+    }
 
     @GetMapping
-    fun get(): List<UserDTO> {
-        return userService.get()
-    }
+    fun get(): List<UserDTO> = userService.get()
 
-    @GetMapping("/{id}")
-    fun getById(@PathVariable id: Long): UserDTO {
-        return userService.get(id)
-    }
+    @GetMapping(ID)
+    fun getById(@PathVariable id: Long): UserDTO = userService.get(id)
 
     @PutMapping
-    fun put(@RequestBody dto: UpdateUserDTO): UserDTO {
-        return userService.put(dto)
+    fun put(@RequestBody @Valid dto: UpdateUserDTO): ResponseEntity<UserDTO> {
+        val userDto = userService.put(dto)
+        return ResponseEntity.ok(userDto)
     }
 
-    @PostMapping
+    @DeleteMapping(ID)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Long) = userService.delete(id)
+
+    @PostMapping(SING_UP)
     fun singUp(
         @RequestBody @Valid dto: SingUpDTO,
         uriBuilder: UriComponentsBuilder
     ): ResponseEntity<UserDTO>{
-        val userDTO = userService.singUp(dto)
-        val uri = uriBuilder.path("/user/${userDTO.id}").build().toUri()
-        return ResponseEntity.created(uri).body(userDTO)
+        val userDto = userService.singUp(dto)
+        val uri = uriBuilder.path(REQUEST.plus(userDto.id)).build().toUri()
+        return ResponseEntity.created(uri).body(userDto)
     }
 }
